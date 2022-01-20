5f5c4e14fdf54188513f0e834cf30c6a6b5cb989
blarth <jminocente@gmail.com>
2022-01-20 16:41:19 -0300

    init and last commit

---
+++ days.js
@@ -0,0 +1,18 @@
+
+
+
+  const holidays = [
+    { date: "1/1/2022", name: "Confraternização mundial", month: "1"  },
+    { date: "3/1/2022", name: "Carnaval", month: "3" },
+    { date: "4/17/2022", name: "Páscoa", month: "4" },
+    { date: "4/21/2022", name: "Tiradentes", month: "4" },
+    { date: "5/1/2022", name: "Dia do trabalho", month: "5" },
+    { date: "6/16/2022", name: "Corpus Christi", month: "6" },
+    { date: "9/7/2022", name: "Independência do Brasil", month: "9" },
+    { date: "10/12/2022", name: "Nossa Senhora Aparecida", month: "10" },
+    { date: "11/2/2022", name: "Finados", month: "11" },
+    { date: "11/15/2022", name: "Proclamação da República", month: "11" },
+    { date: "12/25/2022", name: "Natal", month: "12" }
+  ];
+
+  export default holidays
---
+++ index.js
@@ -0,0 +1,67 @@
+import holidays from './days.js';
+import express from 'express';
+import cors from 'cors';
+
+
+const today = new Date();
+
+const app = express();
+app.use(cors());
+
+
+
+
+function checkHoliday(holidays){
+    for(let i= 0; i < holidays.length; i++){
+        if(holidays[i].date.includes(today.toLocaleDateString()) ){
+           return "Sim, hoje é nome-do-feriado"
+            
+        }
+    }
+    return "Não, hoje não é feriado"
+}
+
+function handleMonth(str){
+    let arrayHollidays = []
+    for(let i= 0; i < holidays.length; i++){
+        if(holidays[i].month === str ){
+            arrayHollidays.push(holidays[i])
+           
+            
+        }
+    }
+    if(arrayHollidays.length === 0){
+        return "Não tem feriado no mes selecionado"
+    }
+    
+    return arrayHollidays
+
+}
+
+
+
+app.get("/holidays", (req, res) => {
+    
+    res.send(holidays);
+})
+
+
+app.get("/is-today-holiday", (req, res) => {
+    let bollens = checkHoliday(holidays)
+
+    res.send(bollens)
+})
+
+
+
+app.get('/holidays/:idHoliday', (req, res) => {
+    const id = req.params.idHoliday;
+    const response = handleMonth(id)
+    res.send(response)
+
+  });
+
+
+app.listen(3000);
+
+
